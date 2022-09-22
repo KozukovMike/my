1e527798d0fd7a3f83a624baa36c75030df351df
Kozukovmike <kozukovmisa@gmail.com>
2022-09-22 19:07:22 +0300

    3_3

---
+++ 3/3_3.py
@@ -0,0 +1,5 @@
+name, age, town = input(), input(), input()
+print(f'Hello {name}, ur age is {age} and u live in {town}')
+print('Hello {}, ur age is {} and u live in {}'.format(name, age, town))
+print('Hello %s, ur age is %s and u live in %s' % (name, age, town))
+
